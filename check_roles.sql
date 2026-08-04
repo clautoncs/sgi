4f607ed3a064fe33cffc451065b99fae606f1335
Clauton <clauton.cs2@gmail.com>
2026-08-04 17:03:26 +0000

    chore: remover scripts temporários de setup

--- check_roles.sql
+++
@@ -1,2 +0,0 @@
-SELECT id, name, description FROM roles;
-SELECT id, name, email, roleId FROM users;
--- update_user.sql
+++
@@ -1,1 +0,0 @@
-UPDATE users SET role = 'sysadmin', name = 'Clauton Sobral' WHERE email = 'admin@ilinked.com.br';
